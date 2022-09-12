16ec6f1f3f329efe937789fc99af0b292c0c2c93
hector sierra <hector360_m@hotmail.com>
2022-09-12 03:00:15 -0500

    uploading frontend changes2

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const SERVER_URL =
-  process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
+"https://yaydoo-backend.herokuapp.com" || "http://localhost:3001"; 
+//   process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
